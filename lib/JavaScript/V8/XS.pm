52a82120f7992ec7132b130a2423b6fd3835634d
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2019-08-14 11:34:07 +0200

    Bump version

--- lib/JavaScript/V8/XS.pm
+++ lib/JavaScript/V8/XS.pm
@@ -7,7 +7,7 @@ use JSON::PP;
 use Text::Trim qw(trim rtrim);
 use XSLoader;
 
-our $VERSION = '0.000013';
+our $VERSION = '0.000014';
 XSLoader::load( __PACKAGE__, $VERSION );
 
 our @EXPORT_OK = qw[];
@@ -49,7 +49,7 @@ JavaScript::V8::XS - Perl XS binding for the V8 Javascript embeddable engine
 
 =head1 VERSION
 
-Version 0.000013
+Version 0.000014
 
 =head1 SYNOPSIS
 
